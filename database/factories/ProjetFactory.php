333b850db6fef7bf6ae763f5a93dc9f0b2c87118
werfellie-emna <57862224+werfellie-emna@users.noreply.github.com>
2020-04-27 22:49:11 +0200

    Add files via upload
    creation de la base/tables projets qui contient les sujets Pfe

---
+++ database/factories/ProjetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Projet;
+use Faker\Generator as Faker;
+
+$factory->define(Projet::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_04_23_234322_create_projets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titre');
+            $table->string('language');
+            $table->string('description');
+            $table->string('date_deb');
+            $table->string('date_fin');
+            $table->string('entreprise');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projets');
+    }
+}
